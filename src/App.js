8bf116d5fc99efe8e7ed9e59a6d531b998f2c0bf
Moses Nyambu <nyamosekitando@gmail.com>
2020-01-16 17:16:14 +0300

    add contact to routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navv from './components/navbar'
 import Home from './components/home';
 import About from './components/about';
 import Portfolio from './components/portfolio';
+import Contact from './components/contact';
 
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
@@ -16,6 +17,9 @@ function App() {
             <Route path='/' component={Home} exact />
             <Route path='/about' component={About} />
             <Route path='/portfolio' component={Portfolio} />
+            <Route path='/contact' component={Contact} />
+            <Route component={Home} />
+
 
           </Switch>      
       </BrowserRouter>
