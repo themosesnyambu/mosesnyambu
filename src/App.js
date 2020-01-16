cbbd013322504c9d28d40c1b8434e226ffe41e81
Moses Nyambu <nyamosekitando@gmail.com>
2020-01-16 16:49:58 +0300

    home added to router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Navv from './components/navbar'
+import Home from './components/home';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div>
-      <Navv />
+    <div style={{ backgroundColor: '#fff' }}>
+      <BrowserRouter>
+                  <Navv />
+          <Switch>
+            <Route path='/' component={Home} exact />
+          </Switch>      
+      </BrowserRouter>
     </div>
   );
 }
