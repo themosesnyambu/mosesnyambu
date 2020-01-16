4755bc68fc9642a99b36c5ba8ca2e7d746b9a76b
Moses Nyambu <nyamosekitando@gmail.com>
2020-01-16 16:47:35 +0300

    created home component

---
+++ src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Home = () => {
+    return (
+        <section className="section">
+            <div
+                className="header slide_in_left"
+                style={{ position: "relative" }}
+            >
+                <h4>I am a Full Stack Developer.<br /> Fascinated by technology and a life-long learner.</h4>
+            </div>
+        </section>
+    );
+}
+
+
+export default Home;
